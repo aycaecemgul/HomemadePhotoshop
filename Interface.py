111973f8f99bb404121f18f2eee838bc36fa4538
aycaecemgul <aycaecemgul@gmail.com>
2020-11-25 13:51:39 +0300

    first

---
+++ Interface.py
@@ -0,0 +1,218 @@
+import base64
+import base64
+import io
+import os
+import PIL
+from PIL import Image
+import PySimpleGUI as sg
+import cv2 as cv
+import numpy as np
+from numpy import asarray
+from skimage.transform import rotate
+import Main
+
+
+def main():
+    sg.theme("Black")
+
+    # Define the window layout
+    layout = [
+        [sg.Text("Image Processing Project 1 By Ayça Ecem Gül", size=(60, 1), justification="center")],
+        [sg.Text('Choose an image', size=(14, 1)), sg.Input(key='-FILE-', enable_events=True), sg.FileBrowse()],
+        [sg.Image(key='-IMAGE-',enable_events=True)],
+        [sg.Image(key='-IMAGE1-',enable_events=True)],
+        [
+            sg.Text("Rotate",size=(14,1)),
+            sg.Button("90",size=(8,1),key="-ROT-90-",enable_events=True),
+            sg.Button("180", size=(8, 1),key="-ROT-180-", enable_events=True),
+            sg.Button("270",size=(8, 1),key="-ROT-270-", enable_events=True)
+        ],
+        [
+            sg.Text("Resize", size=(14, 1)),
+            sg.Input(key="-X-",size=(8,1)),sg.Input(key="-Y-",size=(8,1)),
+            sg.Button("Apply", size=(8, 1), key="-RESIZE-APPLY-", enable_events=True)
+        ],
+        [
+            sg.Text("Rescale",size=(14, 1)),
+            sg.Input(key="-RESCALE-AMOUNT-", size=(8, 1)),
+            sg.Button("Apply", size=(8, 1), key="-RESCALE-APPLY-", enable_events=True)
+        ],
+        [
+            sg.Text("Flip",size=(14,1)),
+            sg.Button("Horizontal",key="-HOR-FLIP-", size=(8, 1), enable_events=True),
+            sg.Button("Vertical",key="-VER-FLIP-" ,size=(8, 1), enable_events=True)
+        ],
+        [
+            sg.Text("Crop", size=(14, 1)),
+            sg.Input(key="-CROPX1-",size=(5,1)),sg.Input(key="-CROPX2-",size=(5,1)),sg.Input(key="-CROPY1-",size=(5,1)),sg.Input(key="-CROPY2-",size=(5,1)),
+            sg.Button("Apply", size=(8, 1), key="-CROP-APPLY-", enable_events=True)
+        ],
+        [
+            sg.Text("Swirl", size=(14, 1)),
+            sg.Slider(
+                (0, 255),
+                128,
+                1,
+                orientation="h",
+                size=(20, 10),
+                key="-SWIRL SLIDER-",
+            ),
+            sg.Button("Apply", size=(8, 1), key="-SWIRL-APPLY-", enable_events=True)
+        ],
+        [
+            sg.Text("Histogram Equalization",size=(24,1))
+        ],
+        [
+            sg.Text("Choose an image to equalize" ,size=(24, 1)), sg.Input(key='-FILE2-', enable_events=True), sg.FileBrowse()
+        ],
+        [sg.Text("Try Ayça's special instagram filter!",size=(27,1)),
+         sg.Button("Apply",size=(8, 1), key="-INSTA-APPLY-",enable_events=True)],
+        # [sg.Radio("None", "Radio", True, size=(10, 1))],
+        # [
+        #     sg.Radio("threshold", "Radio", size=(10, 1), key="-THRESH-"),
+        #     sg.Slider(
+        #         (0, 255),
+        #         128,
+        #         1,
+        #         orientation="h",
+        #         size=(40, 15),
+        #         key="-THRESH SLIDER-",
+        #     ),
+        # ],
+        # [
+        #     sg.Radio("canny", "Radio", size=(10, 1), key="-CANNY-"),
+        #     sg.Slider(
+        #         (0, 255),
+        #         128,
+        #         1,
+        #         orientation="h",
+        #         size=(20, 15),
+        #         key="-CANNY SLIDER A-",
+        #     ),
+        #     sg.Slider(
+        #         (0, 255),
+        #         128,
+        #         1,
+        #         orientation="h",
+        #         size=(20, 15),
+        #         key="-CANNY SLIDER B-",
+        #     ),
+        # ],
+        # [
+        #     sg.Radio("blur", "Radio", size=(10, 1), key="-BLUR-"),
+        #     sg.Slider(
+        #         (1, 11),
+        #         1,
+        #         1,
+        #         orientation="h",
+        #         size=(40, 15),
+        #         key="-BLUR SLIDER-",
+        #     ),
+        # ],
+        # [
+        #     sg.Radio("hue", "Radio", size=(10, 1), key="-HUE-"),
+        #     sg.Slider(
+        #         (0, 225),
+        #         0,
+        #         1,
+        #         orientation="h",
+        #         size=(40, 15),
+        #         key="-HUE SLIDER-",
+        #     ),
+        # ],
+        # [
+        #     sg.Radio("enhance", "Radio", size=(10, 1), key="-ENHANCE-"),
+        #     sg.Slider(
+        #         (1, 255),
+        #         128,
+        #         1,
+        #         orientation="h",
+        #         size=(40, 15),
+        #         key="-ENHANCE SLIDER-",
+        #     ),
+        # ],
+        [sg.Button("Save", size=(8, 1)),
+         sg.Button("Exit", size=(8, 1))]
+    ]
+
+    # Create the window and show it without the plot
+    window = sg.Window("OpenCV Integration", layout, location=(800, 400),resizable=True)
+
+    def convert_to_bytes(file_or_bytes, resize=None):
+        '''
+        Will convert into bytes and optionally resize an image that is a file or a base64 bytes object.
+        Turns into  PNG format in the process so that can be displayed by tkinter
+        :param file_or_bytes: either a string filename or a bytes base64 image object
+        :type file_or_bytes:  (Union[str, bytes])
+        :param resize:  optional new size
+        :type resize: (Tuple[int, int] or None)
+        :return: (bytes) a byte-string object
+        :rtype: (bytes)
+        '''
+        if isinstance(file_or_bytes, str):
+            img = PIL.Image.open(file_or_bytes)
+        else:
+            try:
+                img = PIL.Image.open(io.BytesIO(base64.b64decode(file_or_bytes)))
+            except Exception as e:
+                dataBytesIO = io.BytesIO(file_or_bytes)
+                img = PIL.Image.open(dataBytesIO)
+
+        cur_width, cur_height = img.size
+        if resize:
+            new_width, new_height = resize
+            scale = min(new_height / cur_height, new_width / cur_width)
+            img = img.resize((int(cur_width * scale), int(cur_height * scale)), PIL.Image.ANTIALIAS)
+        bio = io.BytesIO()
+        img.save(bio, format="PNG")
+        del img
+        return bio.getvalue()
+
+    while True:
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Exit'):
+            break
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == '-FILE-':
+            filename = values['-FILE-'] #!!!!!!!!!!
+            window['-IMAGE-'].update(data=convert_to_bytes(filename, resize=(400,400)))
+
+        # elif values["-THRESH-"]:
+        #     if filename != None:
+        #         image=Image.open(filename)
+        #         frame = asarray(image)
+        #         window['-IMAGE-'].update(data=convert_to_bytes(frame, resize=(400, 400)))
+        # elif values["-CANNY-"]:
+        #     frame = cv2.Canny(
+        #         frame, values["-CANNY SLIDER A-"], values["-CANNY SLIDER B-"]
+        #     )
+        elif event == "-APPLY-":
+
+            degree = int(values["-ROTATE-SLIDER-"])
+            src = Image.open(filename)
+            src=asarray(src)
+            rotated_image_array=rotate(src,degree,resize=True)
+            filename = filename[:-4]
+            filename+="-rotated.png"
+            rotated_image = Image.fromarray(rotated_image_array, 'RGB')
+            rotated_image.save(filename)
+            rotated_image.show()
+            window['-IMAGE-'].update(data=convert_to_bytes(filename,resize=(400,400)))
+        # elif values["-HUE-"]:
+        #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        #     frame[:, :, 0] += int(values["-HUE SLIDER-"])
+        #     frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+        # elif values["-ENHANCE-"]:
+        #     enh_val = values["-ENHANCE SLIDER-"] / 40
+        #     clahe = cv2.createCLAHE(clipLimit=enh_val, tileGridSize=(8, 8))
+        #     lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+        #     lab[:, :, 0] = clahe.apply(lab[:, :, 0])
+        #     frame = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+        #
+        # imgbytes = cv2.imencode(".png", frame)[1].tobytes()
+        # window["-IMAGE-"].update(data=imgbytes)
+
+    window.close()
+
+main()
---
+++ Main.py
@@ -0,0 +1,97 @@
+import skimage
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import data, io, filters, feature, exposure, color, util, img_as_float
+import skimage.io as io
+from skimage.morphology import closing,square
+from skimage.color import rgb2gray
+from skimage.transform import rotate,swirl,rescale, resize
+import scipy
+from scipy import misc
+import cv2 as cv
+from skimage.util import crop
+from skimage.exposure import match_histograms
+
+reference = data.coffee()
+image = data.chelsea()
+
+#goruntu iyileştirme islemleri,filtreler (10 farklı filtre icermeli)
+
+
+
+#histogram equalization resimleri gösterme
+def equalize_histogram(image, reference):
+    matched_image = match_histograms(image, reference, multichannel=True)
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3))
+    ax1.imshow(image)
+    ax1.set_title('Source')
+    ax2.imshow(reference)
+    ax2.set_title('Reference')
+    ax3.imshow(matched_image)
+    ax3.set_title('Matched Image')
+
+    plt.tight_layout()
+    plt.show()
+    return matched_image
+
+
+#histogram eşitleme grafiği gösterme
+def plot_equalized_histogram(image,reference,matched_image):
+
+    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(8, 8))
+
+    for i, img in enumerate((image, reference, matched_image)):
+        for c, c_color in enumerate(('red', 'green', 'blue')):
+            img_hist, bins = exposure.histogram(img[..., c], source_range='dtype')
+            axes[c, i].plot(bins, img_hist / img_hist.max())
+            img_cdf, bins = exposure.cumulative_distribution(img[..., c])
+            axes[c, i].plot(bins, img_cdf)
+            axes[c, 0].set_ylabel(c_color)
+
+    axes[0, 0].set_title('Source')
+    axes[0, 1].set_title('Reference')
+    axes[0, 2].set_title('Matched')
+
+    plt.tight_layout()
+    plt.show()
+
+
+#resize,rotation,cropping,swirling, gibi 5 farklı donusum islemi
+def rotate_image(image,degree):
+
+    rotated_image=rotate(image,degree)
+    return rotated_image
+
+
+
+def resize_image(image,amount):
+    image_resized = resize(image, (amount[0],amount[1]), anti_aliasing=True)
+    io.imshow(image_resized)
+    io.show()
+
+def swirl_image(image,strength,radius):
+    swirled_image = swirl(image, rotation=0, strength=strength, radius=radius)
+    io.imshow(swirled_image)
+    io.show()
+
+def crop_image(image,crop_width):
+    cropped_image=crop(image,crop_width,copy=True)
+    io.imshow(cropped_image)
+    io.show()
+
+def rescale_image(image,amount):
+    rescaled_image=rescale(image,amount, anti_aliasing=False)
+    io.imshow(rescaled_image)
+    io.show()
+
+#yogunluk donusumu işlemleri (degerleri kullanıcı verebilmeli)
+
+#morfolojik işlemler (10 farklı morfolojik işlem icermeli)
+
+
+#aşındırma(erosion)
+def erosion():
+    print("")
+
+#insta filtresi oluştur.
+
